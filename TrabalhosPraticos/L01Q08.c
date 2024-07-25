63f015042390f0a589768ff1a15f88d266ec6696
BryanYunche <estudanteporesporte@gmail.com>
2024-07-25 09:11:58 -0300

    começo da questão 08

---
+++ TrabalhosPraticos/L01Q08.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Ler o valor de um lado do quadrado  e apresentar: PERÍMETRO=xx, ÁREA=xx e DIAGONAL=xx 
+
+int main(){
+
+    int ladoQuadrado;
+    double area, perimetro, diagonal;
+
+    printf();
+}
