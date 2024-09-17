9c42780df45ce84f99046b9c410b9b33eb55301d
BryanYunche <estudanteporesporte@gmail.com>
2024-09-17 09:23:13 -0300

    Atualização git

---
+++ TrabalhosPraticos/AlgoritmosEm_Python_/Lista_01/Q01.py
@@ -0,0 +1,18 @@
+    # Criar 3 variáveis (espaços de memória) de nome M1, M2 e M3, para armazenar valores inteiros;
+    
+
+    #Armazenar a constante 10 na posiçãoo de memória M3;
+
+    #Ler um valor inteiro colocando-o na posiçãoo de memória M1;
+
+    #Armazenar na posição de memória M2 o conteúdo da posição de memória M1 mais 8 unidades;
+
+    #Apresentar o conteúdo das posições de memória M1, M2 e M3, identificando-os;
+
+    #Adicionar o conteúdo das posições de memória M1 e M2 a posição de memória M3;
+
+    #Armazenar na posição de memória M3 o triplo de sua metade;
+
+    #Adicionar uma unidade a posição de memória M3;
+
+    #Apresentar o conteúdo da posição de memória M3, identificando-o;
