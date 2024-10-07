37d7b09e20912b68e3a312481bc69e7aff68b8dc
BryanYunche <estudanteporesporte@gmail.com>
2024-10-07 14:57:03 -0300

    Começando a revisão

--- Revisao/listas.py
+++ Revisao/listas.py
@@ -1,3 +1,11 @@
+#Listas: É uma estrutura de dados que armazena elementos em uma sequência. 
+# Cada elemento tem uma posição específica, e é possível acessar diretamente os elementos pela sua posição.
+
+#Exemplo:
+print(20*'=')
+lista = [1, 2, 3, 4, 5, 6, 7, 8]
+print(lista)
+
 #Encadeadas
 #Recursivas
 #Circulares
